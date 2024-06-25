a335671aa75112e258bee7c6de1c5f2a20336362
Suzry <mohmmadfahad1919@gmail.com>
2024-06-25 06:35:08 +0300

    myproject

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    
+    <title>Calculator</title>
+
+</svelte:head>
+
+
+<div class="bg-[#e6e7eb] min-h-[100dvh] justify-center flex items-center ">
+
+<div class= "bg-white w-[20rem] h-[30rem] rounded-3xl justify-center flex items-center">
+    <div class="bg-blue-500 w-[6rem] h-[6rem] rounded-full flex justify-center items-center ">why should we put it to the first </div>
+
+</div>
+
+</div>
